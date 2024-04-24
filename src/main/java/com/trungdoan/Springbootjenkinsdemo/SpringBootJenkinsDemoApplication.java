d30b149ea72e07d37f783d710d2ee576b2ff5373
trung doan <doant.anthony@gmail.com>
2024-04-24 14:26:52 -0400

    initial commit

---
+++ src/main/java/com/trungdoan/Springbootjenkinsdemo/SpringBootJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.trungdoan.Springbootjenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkinsDemoApplication.class, args);
+	}
+
+}
